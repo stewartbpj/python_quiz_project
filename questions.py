949a22676295d1c95fdc266159173dbc23fa7c44
Ben <stewart.bpj@gmail.com>
2024-12-26 23:43:15 -0700

    added more multichoice

--- questions.py
+++ questions.py
@@ -1,28 +1,27 @@
-def questions = [ "
-    "If a company has 2 security guards that control an access vestibule, and the company has a SCIF that is only accessible with an 
-    RFID card, what type two types of security controls are being implemented?", 
-
-    "If a server has sufferred an attack, the IT admin is searching through the log files trying to determine and understand what happened, 
-    what type of security control is being implemented?",
-
+questions = [
+    "If a server has suffered an attack, the IT admin is searching through the log files trying to determine and understand what happened, what type of security control is being implemented?",
     "What kind of control is applied when an IT manager tasks an employee with developing standard operating procedures?",
-
     "Eliminating threats and reducing potential threats is what type of security control?",
-
     "What is it called when you are performing updates that are critical to your system and the updates are released by the vendor?",
-
     "There was a recent ransomware attack, technical controls were compromised, what type of control should be implemented to prevent this from happening again?",
+    "Following an audit, there was recommendation that additional instructions needed to be included in the current policy, what type of control is being applied?"
+]
 
-    "Following an audit, there was recommendation that additonal instructions needed to be included in the current policy, what type of control is being applied?",
+answerKey = [
+    "Deterrent and Physical",
+    "Investigative",
+    "Directive",
+    "Preventative",
+    "Deterrent",
+    "Compensating",
+    "Directive"
+]
 
- " ]
 
- def answerKey = [ 
-   "Deterrent and Phyiscal", 
-   "Investigative", 
-   "Directive", 
-   "Preventative", 
-   "Deterrent", 
-   "Compensating", 
-   "Directive"
-   ]
+multiChoice = [
+  a. "Deterrent and Physical",
+  b. "Investigative",
+  c. "Directive",
+  d. "Preventative",
+  e. "Deterrent",
+  f. "Compensating",]
